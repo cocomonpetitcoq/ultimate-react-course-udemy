f73f7d433efdba9afe71c9160ec9e3d42a5c2232
Angela Martel <6212215@cshawi.ca>
2024-04-09 18:42:00 -0400

    date counter project completed date does not calculate perfectly

--- 04-steps/date-counter/src/App.js
+++ 04-steps/date-counter/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="App">
+            <Counter />
+        </div>
+    );
 }
 
-export default App;
+function Counter() {
+    const today = new Date();
+    const [count, setCount] = useState(1);
+    const [step, setStep] = useState(1);
+
+    function handleLowerStep() {
+        if (step > 1) {
+            setStep((currentStep) => currentStep - 1);
+        }
+    }
+
+    function handleHigerStep() {
+        setStep((currentStep) => currentStep + 1);
+    }
+
+    function handleLowerCount() {
+        setCount((currentCount) => currentCount - step);
+    }
+
+    function handleHigherCount() {
+        setCount((currentCount) => currentCount + step);
+    }
+
+    return (
+        <div>
+            <div>
+                <button onClick={handleLowerStep}>-</button>
+                <p>Step: {step}</p>
+                <button onClick={handleHigerStep}>+</button>
+            </div>
+            <div>
+                <button onClick={handleLowerCount}>-</button>
+                <p>Count: {count}</p>
+                <button onClick={handleHigherCount}>+</button>
+            </div>
+            <div>
+                {count === 0 && <p>Today is {today.toLocaleDateString()};</p>}
+                {count > 0 && (
+                    <p>
+                        {count} {count > 1 ? "days" : "day"} from today is {new Date(today + count * 1000 * 60 * 60 * 24).toLocaleDateString()}
+                    </p>
+                )}
+                {count < 0 && (
+                    <p>
+                        {count} {count < -1 ? "days" : "day"} ago was {new Date(today - count * 1000 * 60 * 60 * 24).toLocaleDateString()}
+                    </p>
+                )}
+            </div>
+        </div>
+    );
+}
