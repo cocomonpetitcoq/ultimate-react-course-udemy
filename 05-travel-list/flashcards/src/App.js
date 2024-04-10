6e7b73d689a0867329026745392554690060bb86
Angela Martel <6212215@cshawi.ca>
2024-04-09 20:45:50 -0400

    flashcards project completed

--- 05-travel-list/flashcards/src/App.js
+++ 05-travel-list/flashcards/src/App.js
@@ -1,47 +1,64 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default function App() {
-	return (
-    <div className="App">
-      <FlashCards />
-    </div>
-  );
+    return (
+        <div className="App">
+            <FlashCards />
+        </div>
+    );
 }
 
 const questions = [
-  {
-    id: 3457,
-    question: "What language is React based on?",
-    answer: "JavaScript"
-  },
-  {
-    id: 7336,
-    question: "What are the building blocks of React apps?",
-    answer: "Components"
-  },
-  {
-    id: 8832,
-    question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX"
-  },
-  {
-    id: 1297,
-    question: "How to pass data from parent to child components?",
-    answer: "Props"
-  },
-  {
-    id: 9103,
-    question: "How to give components memory?",
-    answer: "useState hook"
-  },
-  {
-    id: 2002,
-    question:
-      "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element"
-  }
+    {
+        id: 3457,
+        question: "What language is React based on?",
+        answer: "JavaScript",
+    },
+    {
+        id: 7336,
+        question: "What are the building blocks of React apps?",
+        answer: "Components",
+    },
+    {
+        id: 8832,
+        question: "What's the name of the syntax we use to describe a UI in React?",
+        answer: "JSX",
+    },
+    {
+        id: 1297,
+        question: "How to pass data from parent to child components?",
+        answer: "Props",
+    },
+    {
+        id: 9103,
+        question: "How to give components memory?",
+        answer: "useState hook",
+    },
+    {
+        id: 2002,
+        question: "What do we call an input element that is completely synchronised with state?",
+        answer: "Controlled element",
+    },
 ];
 
 function FlashCards() {
-  return <div>TODO</div>;
+    const [key, setKey] = useState(null);
+
+    function handleSelection(id) {
+        if (key === id) {
+            setKey(null);
+        } else {
+            setKey(id);
+        }
+    }
+
+    return (
+        <div className="flashcards">
+            {questions.map((question) => (
+                <div className={key === question.id ? "selected" : ""} onClick={() => handleSelection(question.id)}>
+                    {key === question.id ? question.answer : question.question}
+                </div>
+            ))}
+        </div>
+    );
 }
